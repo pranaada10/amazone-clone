5680b70ca95cb7460a77be0d44f1c0db8942377b
Deepa Nagouda <deepanagouda10@gmail.com>
2026-07-03 15:31:51 +0530

    Connected MongoDB configuration

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
-
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -9,7 +14,7 @@ app.get("/", (req, res) => {
     res.send("Amazon Backend API is running...");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
